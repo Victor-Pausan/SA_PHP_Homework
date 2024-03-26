dea91ba6eacc0d70d6363d3f434ca3710d915f35
Victor-Pausan <pausanvictor@gmail.com>
2024-03-26 11:34:44 +0200

    initialized the create acc and log in system

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -3,6 +3,6 @@
 $serverName = "localhost";
 $dbUsername = "root";
 $dbPassword = "rahan2003";
-$dbName = "classes_php_hw";
+$dbName = "users_php_hw";
 
 $pdo = new PDO("mysql: $serverName;port=8888; dbname=$dbName", $dbUsername, $dbPassword);
